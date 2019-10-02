1a7d1b77700fcf28ebd4adb87cb39b654c2cdd3d
McLarenCollege <hello@mclarencollege.in>
2019-10-02 08:10:29 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,27 @@
+
+void main(){
+  List<String> names = ["Kimiko", "Raj", "Anu", "Paul"];
+
+// Write a program to allow user to modify the given list as shown below.
+// Add Student, remove student and stop the program
+
+}
+
+//[Kimiko, Raj, Anu, Paul]
+//(a)dd or (r)emove a student, or (e)xit?
+//a
+//What is the name of the student you want to add?
+//Arnav
+//[Kimiko, Raj, Anu, Paul, Arnav]
+//(a)dd or (r)emove a student, or (e)xit?
+//r
+//What is the name of the student you want to remove?
+//Raj
+//[Kimiko, Anu, Paul, Arnav]
+//(a)dd or (r)emove a student, or (e)xit?
+//r
+//What is the name of the student you want to remove?
+//Paul
+//[Kimiko, Anu, Arnav]
+//(a)dd or (r)emove a student, or (e)xit?
+//e
